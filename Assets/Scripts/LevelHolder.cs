bbf550c0012c5808dc883fbbef6bd8821aaeb277
Devon R <sansgorialis@gmail.com>
2021-04-24 15:13:07 +0900

    Initial level stuff

--- Assets/Scripts/LevelHolder.cs
+++ Assets/Scripts/LevelHolder.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class LevelHolder : MonoBehaviour
 {
-    int currentLevel = 1;
-
     List<GameObject> levelObjects = new List<GameObject>();
     List<Vector3> levelVelocities = new List<Vector3>();
 
     // We want to smoothly 'drop' the current level when it is completed
     // Ideally, this constitutes destroying it as well, for performance and, well, the sake of ease
     // Each level object is interpolated towards a target, based on where it is in the upcoming level buffer.
-    // When the player reaches the end of a level, changingLevel becomes true, and changingLevelProgress tracks the time since this happened.
-    // Thus, when changingLevel is true, the interpolation target for each level changes to being 1 in advance, creating the movement.
-    // After changingLevelProgress has reached a certain amount, the first level (the completed one) is destroyed and changingLevel becomes false again.
-    bool changingLevel = false;
-    float changingLevelProgress = 0.0f;
+    // When the player reaches the end of a level, the currently dropped level is destroyed.
+    // This causes the interpolation targets for every subsequent level to change.
 
     // Adjustable smoothing, game feel variable
     public float m_Smoothing = 0.5f;
@@ -29,7 +24,6 @@ public class LevelHolder : MonoBehaviour
         {
             levelObjects.Add(child.gameObject);
             levelVelocities.Add(Vector3.zero);
-            Debug.Log(child.gameObject);
         }
     }
 
@@ -41,22 +35,23 @@ public class LevelHolder : MonoBehaviour
             GameObject level = levelObjects[i];
             Vector3 velocity = levelVelocities[i];
 
-            int j = i - (changingLevel ? 1 : 0);
-            Vector3 targetPosition = new Vector3(0.0f, j < 0 ? -50.0f : 0.0f, j * 15.0f);
+            Vector3 targetPosition = new Vector3(0.0f, i == 0 ? -50.0f : 0.0f, (i - 1) * 15.0f);
 
             level.transform.position = Vector3.SmoothDamp(level.transform.position, targetPosition, ref velocity, m_Smoothing);
         }
-
-        changingLevelProgress = changingLevel ? changingLevelProgress + Time.deltaTime : 0.0f;
     }
 
     public float GetLevelDepth()
     {
-        return levelObjects[changingLevel ? 1 : 0].transform.position.z;
+        return levelObjects[1].transform.position.z;
     }
 
     public void OnLevelComplete()
     {
-        changingLevel = true;
+        // Destroy the first level in the sequence
+        GameObject.Destroy(levelObjects[0]);
+        levelObjects.RemoveAt(0);
+        levelVelocities.RemoveAt(0);
+
     }
 }
